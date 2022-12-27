5fad8ac1e64fc1a5bb3ceb4697ec46055f4f6a4b
fajar035 <ikehikeh151@gmail.com>
2022-12-27 19:07:52 +0700

    section about & responsive

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
-// import { UilFacebook, UilInstagram, UilWhatsapp, UilGithub } from '@iconscout/react-unicons';
+import { UilDownloadAlt } from '@iconscout/react-unicons';
+import photoAbout from '../../assets/images/foto4.jpg';
 import './styles.css';
 
 function About() {
   return (
     <section className="container-about" id="about">
-      <p>About</p>
+      <h1>About</h1>
+      <p>My Introdaction</p>
+      <div className="wrapper-profile">
+        <div className="container-wrapper-photo">
+          <div className="wrapper-photo">
+            <img src={photoAbout} alt="profile-about" />
+          </div>
+        </div>
+        <div className="wrapper-profile-data">
+          <p>
+            1 year more after graduating from my university into the world of web development.
+            Becoming a web Developer was my dream from an early age. Currently, I&apos;m focused on
+            learning the backend developer php language to achieve my desired goal.
+          </p>
+
+          <div className="container-experience">
+            <div className="wrapper-experience">
+              <h1>01+</h1>
+              <p>Years experience</p>
+            </div>
+
+            <div className="wrapper-experience">
+              <h1>01+</h1>
+              <p>Years experience</p>
+            </div>
+
+            <div className="wrapper-experience">
+              <h1>01+</h1>
+              <p>Years experience</p>
+            </div>
+          </div>
+
+          <button type="button" className="btn-download-cv">
+            Download CV
+            <UilDownloadAlt className="icon-btn" />
+          </button>
+        </div>
+      </div>
     </section>
   );
 }
