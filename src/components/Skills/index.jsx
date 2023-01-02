0b0508a4ced1cc6e983678e9237753b7e46e3e18
fajar035 <ikehikeh151@gmail.com>
2023-01-02 17:46:44 +0700

    update header skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
-// import { UilFacebook, UilInstagram, UilWhatsapp, UilGithub } from '@iconscout/react-unicons';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useState } from 'react';
+import { UilBracketsCurly, UilAngleDown, UilServerNetworkAlt } from '@iconscout/react-unicons';
 import './styles.css';
 
 function Skills() {
+  const [openFrontend, setOpenFrontend] = useState(false);
+  const [openBackend, setopenBackend] = useState(false);
+
+  const handleFrontend = () => setOpenFrontend(!openFrontend);
+  const handleBackend = () => setopenBackend(!openBackend);
+
+  useEffect(() => {
+    console.log('SKILLS FRONTEND: ', openFrontend);
+    console.log('SKILLS BACKEND : ', openBackend);
+  }, [openFrontend, openBackend]);
+
   return (
     <section className="container-skills" id="skills">
-      <p>Skills</p>
+      <h1>Skills</h1>
+      <p>My Technical Level</p>
+
+      <div className="wrapper-skills">
+        <div className="header-skills" onClick={handleFrontend}>
+          <div className="left-skills">
+            <UilBracketsCurly className="icon-skills" />
+            <div className="title-skills">
+              <h1>Frontend Developer</h1>
+              <p>2 years experience</p>
+            </div>
+          </div>
+          <UilAngleDown className={`icon-skills ${openFrontend ? 'icon-up' : ''}`} />
+        </div>
+
+        <div className="header-skills" onClick={handleBackend}>
+          <div className="left-skills">
+            <UilServerNetworkAlt className="icon-skills" />
+            <div className="title-skills">
+              <h1>Backend Developer</h1>
+              <p>1 years experience</p>
+            </div>
+          </div>
+          <UilAngleDown className={`icon-skills ${openBackend ? 'icon-up' : ''}`} />
+        </div>
+      </div>
+
+      {/* <div className="data-skills"></div> */}
     </section>
   );
 }
---
+++ src/dummy/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    frontend: {
+      HTML: '100%',
+      CSS: '90%',
+      React: '80%',
+      'React-Native': '70%',
+      NextJS: '75%',
+      NuxtJs: '80%',
+    },
+  },
+  {
+    backend: {
+      MySQL: '70%',
+      NodeJS: '80%',
+      ExpressJS: '80%',
+      PHP: '50%',
+    },
+  },
+];
+
+export default data;
