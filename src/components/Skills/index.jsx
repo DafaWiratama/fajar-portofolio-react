6c8a9434b614e0c9301a4a86f74a0628066a1975
fajar035 <ikehikeh151@gmail.com>
2023-08-08 17:16:41 +0700

    fix: section skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,34 +1,87 @@
-// import React, { useEffect, useState } from 'react';
-// import { UilAngleDown, UilBracketsCurly, UilServerNetworkAlt } from '@iconscout/react-unicons';
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { UilAngleDown, UilBracketsCurly, UilServerNetworkAlt } from '@iconscout/react-unicons';
 import './styles.css';
-// import data from '../../dummy/data';
+import data from '../../dummy/data';
 
 function Skills() {
-  // const [openFrontend, setOpenFrontend] = useState(false);
-  // const [openBackend, setopenBackend] = useState(false);
+  const [openFrontend, setOpenFrontend] = useState(true);
+  const [openBackend, setopenBackend] = useState(true);
+  const [fe, setFe] = useState([]);
+  const [be, setBe] = useState([]);
 
-  // const handleFrontend = () => setOpenFrontend(!openFrontend);
-  // const handleBackend = () => setopenBackend(!openBackend);
+  const handleFrontend = () => setOpenFrontend(!openFrontend);
+  const handleBackend = () => setopenBackend(!openBackend);
 
-  // useEffect(() => {}, [openFrontend, openBackend]);
-  // console.log('fe : ', openFrontend);
-  // console.log('be : ', openBackend);
+  useEffect(() => {
+    setFe(data.data[0].skills);
+    setBe(data.data[1].skills);
+  }, []);
 
   return (
     <section className="section-skills" id="skills">
       <h1>Skills</h1>
-      <p>My technical level</p>
+      <p>My Technical Level</p>
       <div className="wrapper-skills">
-        <div className="skills-content">
-          <div className="skills-header">
-            <h1>Header</h1>
-            <p>Percentage</p>
+        <div className="content_skills" onClick={handleFrontend}>
+          <div className="header_skills">
+            <div className="left-skills">
+              <UilBracketsCurly className="icon-skills" />
+              <div className="title-skills">
+                <h1>Frontend Developer</h1>
+                <p>2 years experience</p>
+              </div>
+            </div>
+            <UilAngleDown className={`icon-skills ${openFrontend ? 'icon-up' : ''}`} />
           </div>
-          <div className="skills-data">
-            <p>Data content</p>
+
+          <ul className={`menu-skills ${openFrontend ? 'menu-active' : ''}`}>
+            {fe.map((item, idx) => {
+              return (
+                <li key={idx}>
+                  <img src={item.img} alt="icons" width={40} className="icon-skills" />
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="content_skills" onClick={handleBackend}>
+          <div className="header_skills">
+            <div className="left-skills">
+              <UilServerNetworkAlt className="icon-skills" />
+              <div className="title-skills">
+                <h1>Backend Developer</h1>
+                <p>1 years experience</p>
+              </div>
+            </div>
+            <UilAngleDown className={`icon-skills ${openBackend ? 'icon-up' : ''}`} />
           </div>
+
+          <ul className={`menu-skills ${openBackend ? 'menu-active' : ''}`}>
+            {be.map((item, idx) => {
+              return (
+                <li key={idx}>
+                  <img src={item.img} alt="icons" width={40} className="icon-skills" />
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
+
+      {/* <div className="wrapper-skills-menu">
+        <div className="menu-skills">
+          <p>Menu Skill</p>
+        </div>
+
+        <div className="menu-skills">
+          <p>Menu Skill</p>
+        </div>
+      </div> */}
     </section>
   );
 }
--- src/dummy/data.js
+++ src/dummy/data.js
@@ -1,3 +1,16 @@
+import html from '../assets/icons/html.png';
+import css from '../assets/icons/css.webp';
+import react from '../assets/icons/react.png';
+import reactNative from '../assets/icons/rn.webp';
+import next from '../assets/icons/nextjs.png';
+import nuxt from '../assets/icons/nuxtjs.webp';
+import express from '../assets/icons/express.png';
+import mysql from '../assets/icons/mysql.png';
+import bootstrap from '../assets/icons/bootstrap.png';
+import tailwind from '../assets/icons/tailwind.webp';
+import vue from '../assets/icons/vue.png';
+import docker from '../assets/icons/docker.png';
+
 const data = [
   {
     id: 'fe',
@@ -5,27 +18,39 @@ const data = [
     skills: [
       {
         name: 'HTML',
-        percentage: '99%',
+        img: html,
       },
       {
         name: 'CSS',
-        percentage: '80%',
+        img: css,
+      },
+      {
+        name: 'Bootstrap',
+        img: bootstrap,
+      },
+      {
+        name: 'Tailwind CSS',
+        img: tailwind,
       },
       {
         name: 'React',
-        percentage: '70%',
+        img: react,
       },
       {
-        name: 'React-Native',
-        percentage: '65%',
+        name: 'Vue JS',
+        img: vue,
       },
       {
-        name: 'NextJS',
-        percentage: '70%',
+        name: 'React Native',
+        img: reactNative,
       },
       {
-        name: 'NuxtJS',
-        percentage: '70%',
+        name: 'Next JS',
+        img: next,
+      },
+      {
+        name: 'Nuxt JS',
+        img: nuxt,
       },
     ],
   },
@@ -35,22 +60,18 @@ const data = [
     skills: [
       {
         name: 'MySQL',
-        percentage: '60%',
+        img: mysql,
       },
       {
-        name: 'NodeJS',
-        percentage: '70%',
+        name: 'Express JS',
+        img: express,
       },
       {
-        name: 'ExpressJS',
-        percentage: '65%',
-      },
-      {
-        name: 'PHP',
-        percentage: '50%',
+        name: 'Docker',
+        img: docker,
       },
     ],
   },
 ];
 
-export default data;
+export default { data };
